1b9875ab959a9f06826ad6e6caacf0e7ed6daa97
wood198 <wood198@mail.chapman.edu>
2018-11-18 19:44:22 -0800

    Fixed customer and employee classes for now. Will need update when the database is set

--- src/CustomerInfo.java
+++ src/CustomerInfo.java
@@ -8,37 +8,40 @@ public class CustomerInfo {
         DBConnect c = new DBConnect();
         Scanner in = new Scanner(System.in);
 
-        //TODO Change the variable names/prompts
         String first_name = "";
         String last_name = "";
-        double gpa = 0.0;
-        String major = "";
-        String advisor = "";
+        String email_input = "";
+        String address_input = "";
+        int phone = 0;
 
-        System.out.println("");
+        System.out.println("Please enter your first name: ");
         first_name = in.nextLine();
-        System.out.println("");
+        System.out.println("Please enter your last name: ");
         last_name = in.nextLine();
-        System.out.println("");
-        gpa = in.nextDouble();
-        in.nextLine(); // Consumes the \n
-        System.out.println("");
-        major = in.nextLine();
-        System.out.println("");
-        advisor = in.nextLine();
+        System.out.println("Please enter your email: ");
+        email_input = in.nextline();
+        System.out.println("Please enter your address: ");
+        address_input = in.nextLine();
+        System.out.println("Pleas enter your phone number with no dashes: ");
+        phone = in.nextInt();
+        in.nextLine(); //consumes the \n
+
 
         //TODO change the SQL statement when we figure out databases
         try {
-            PreparedStatement stat = c.getDBConnection().prepareStatement(" INSERT INTO student (FirstName, LastName, GPA, Major, FacultyAdvisor)"
+            //TODO figure out if thats how TXNs actually work
+            START TRANSACTION;
+            PreparedStatement stat = c.getDBConnection().prepareStatement(" INSERT INTO student (cusFirstName, cusLastName, cusEmail, cusAddress, cusPhone)"
                     + " VALUES (?, ?, ?, ?, ?)");
 
             stat.setString(1, first_name);
             stat.setString(2, last_name);
-            stat.setDouble(3, gpa);
-            stat.setString(4, major);
-            stat.setString(5, advisor);
+            stat.setString(3, email_input);
+            stat.setString(4, address_input);
+            stat.setInt(5, phone);
 
             stat.execute();
+            COMMIT;
 
             System.out.println("New customer added!");
         }catch(SQLException ex){
--- src/EmployeeInfo.java
+++ src/EmployeeInfo.java
@@ -8,39 +8,57 @@ public class EmployeeInfo {
         DBConnect c = new DBConnect();
         Scanner in = new Scanner(System.in);
 
-        //TODO Change the variable names/prompts
-        String  = "";
+        String first_name = "";
         String last_name = "";
-        double gpa = 0.0;
-        String major = "";
-        String advisor = "";
+        String email_input = "";
+        String address_input = "";
+        int phone = 0;
+        int job = 0;
 
-        System.out.println("");
+        System.out.println("Please enter your first name: ");
         first_name = in.nextLine();
-        System.out.println("");
+        System.out.println("Please enter your last name: ");
         last_name = in.nextLine();
-        System.out.println("");
-        gpa = in.nextDouble();
-        in.nextLine(); // Consumes the \n
-        System.out.println("");
-        major = in.nextLine();
-        System.out.println("");
-        advisor = in.nextLine();
+        System.out.println("Please enter your email: ");
+        email_input = in.nextline();
+        System.out.println("Please enter your address: ");
+        address_input = in.nextLine();
+        System.out.println("Pleas enter your phone number with no dashes: ");
+        phone = in.nextInt();
+        in.nextLine();
+        System.out.println("Do you know the ID number of your job? (y/n) ");
+        String jobmenu = in.nextLine();
+
+        if(jobmenu.equals("y")){
+            System.out.println("Job Menu: \n" + "1. Manager \n" + "2. Assistant Manager \n" + "3. Cashier \n" + "4. Warehouse Worker")
+            System.out.println("What is the ID number of your job?" );
+            job = in.nextInt();
+            in.nextLine();
+
+        }else(){
+            System.out.println("What is the ID number of your job?" );
+            job = in.nextInt();
+            in.nextLine();
+        }
 
         //TODO change the SQL statement when we figure out databases
         try {
-            PreparedStatement stat = c.getDBConnection().prepareStatement(" INSERT INTO student (FirstName, LastName, GPA, Major, FacultyAdvisor)"
-                    + " VALUES (?, ?, ?, ?, ?)");
+            //TODO figure out if thats how TXNs actually work
+            START TRANSACTION;
+            PreparedStatement stat = c.getDBConnection().prepareStatement(" INSERT INTO employee (emFirstName, emLastName, emEmail, emAddress, emPhone, idjob)"
+                    + " VALUES (?, ?, ?, ?, ?, ?)");
 
             stat.setString(1, first_name);
             stat.setString(2, last_name);
-            stat.setDouble(3, gpa);
-            stat.setString(4, major);
-            stat.setString(5, advisor);
+            stat.setString(3, email_input);
+            stat.setString(4, address_input);
+            stat.setInt(5, phone);
+            stat.setInt(6, job);
 
             stat.execute();
+            COMMIT;
 
-            System.out.println("New customer added!");
+            System.out.println("New employee added to our system!");
         }catch(SQLException ex){
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
