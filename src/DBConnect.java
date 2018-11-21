a969f92e00dea4572083de696fecce3fe38b9393
wood198 <wood198@mail.chapman.edu>
2018-11-21 13:42:16 -0800

    put the correct connection information

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -11,7 +11,7 @@ public class DBConnect {
 
             }
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaconvert", "ashleynw04", "ashynic2");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/notblockbuster", "ashleynw04", "ashynic2");
 
         } catch (SQLException ex) {
             // handle any errors
