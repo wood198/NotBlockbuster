d4814046dcb2678bbe6e571300165d796ab4f869
wood198 <wood198@mail.chapman.edu>
2018-11-23 18:33:11 -0800

    Search By Movie

--- src/Search.java
+++ src/Search.java
@@ -9,14 +9,31 @@ public class Search {
 
     public void printInStock() throws Exception {
         try{
-            //Print all the movies in stock
-            PreparedStatement stat = c.getDBConnection().prepareStatement("SELECT stockdetails.idmovie, Title\n" +
-                    "  FROM stockdetails JOIN movieforms \n" +
-                    "    ON stockdetails.idmovie = movieforms.idmovie\n" +
-                    " WHERE InStock > 0");
+            System.out.println("Here are the formats movies come in: ");
+            PreparedStatement stat = c.getDBConnection().prepareStatement("SELECT * FROM formats");
             ResultSet rs = stat.executeQuery();
             ResultSetMetaData rsmd = rs.getMetaData();
             int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue);
+                }
+                System.out.println("");
+            }
+
+            int formatID = w.getInt("Which of these formats would you like the movie you rent to be in? ");
+
+            //Print all the movies in stock
+            stat = c.getDBConnection().prepareStatement("SELECT stockdetails.idmovie, Title\n" +
+                    "  FROM stockdetails JOIN movieforms \n" +
+                    "    ON stockdetails.idmovie = movieforms.idmovie\n" +
+                    " WHERE InStock > 0 AND idformat = ?");
+            stat.setInt(1, formatID);
+            rs = stat.executeQuery();
+            rsmd = rs.getMetaData();
+            columnsNumber = rsmd.getColumnCount();
             while (rs.next()) {
                 for (int i = 1; i <= columnsNumber; i++) {
                     if (i > 1) System.out.print(",  ");
@@ -122,5 +139,34 @@ public class Search {
 
     public void searchMovie() throws Exception {
 
+        String movieSearch = w.getString("Title or Keyword: ");
+        try{
+            //Searching by the users terms
+            PreparedStatement stat = c.getDBConnection().prepareStatement("SELECT idmovie, Title FROM stockdetails WHERE Title LIKE ?");
+            stat.setString(1, "%" + movieSearch + "%");
+            ResultSet rs = stat.executeQuery();
+
+            if(rs == null){
+                System.out.println("Sorry there are no movies with that title/keyword.");
+            }else{
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                while (rs.next()) {
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        if (i > 1) System.out.print(",  ");
+                        String columnValue = rs.getString(i);
+                        System.out.print(columnValue);
+                    }
+                    System.out.println("");
+                }
+            }
+
+
+        }catch(SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
     }
 }
